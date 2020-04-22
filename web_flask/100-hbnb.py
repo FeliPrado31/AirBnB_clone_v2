eb378360e38f6c572851e09eb2d5ce89fb8e9af5
Feli <srgatoiscool@gmail.com>
2020-04-22 18:08:51 -0500

    fix pep8

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -13,13 +13,14 @@ def tear_down(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def state_list():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     states = storage.all("State")
     amenities = storage.all("Amenity")
-
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    places = storage.all("Place")
+    users = storage.all("User")
+    return render_template('100-hbnb.html', states=states, amenities=amenities,
+                           places=places, users=users)
 
 
 if __name__ == "__main__":
